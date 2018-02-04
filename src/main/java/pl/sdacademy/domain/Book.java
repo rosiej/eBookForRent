b5d6752a64137b909f852d86d4038a386664971c
Horus07 <marcin.dziedzic02@gmail.com>
2018-02-04 10:53:20 +0100

    add Categories and subcategories

--- src/main/java/pl/sdacademy/domain/Book.java
+++ src/main/java/pl/sdacademy/domain/Book.java
@@ -4,4 +4,10 @@ public class Book {
     private String title;
     private String author;
     private String content;
+
+    public Book(String title, String author, String content) {
+        this.title = title;
+        this.author = author;
+        this.content = content;
+    }
 }
--- src/main/java/pl/sdacademy/domain/EBookStorage.java
+++ src/main/java/pl/sdacademy/domain/EBookStorage.java
@@ -1,35 +1,47 @@
 package pl.sdacademy.domain;
 
-import java.util.ArrayList;
-import java.util.TreeMap;
 
 public class EBookStorage {
-    ArrayList<Category> mainCategory = new ArrayList<Category>();
+    private Category mainCategory;
 
-    Category biograf = new Category("Biografie");
-    Category autobiograf = new Category("Autobiografie");
-    Category jurnal = new Category("Pamietniki");
-    Category Interview = new Category("Wywiady");
-    Category Come dy = new Category("Komedie");
-    Category BlackComedy = new Category("Czarny humor");
-    Category Criminal = new Category("Kryminał");
-    Category Romans = new Category("Komedie romantyczne");
-    Category Horror = new Category("Horror");
+    public EBookStorage() {
+        Category biography = new Category("Biografie", 1);
+        Category forKids = new Category("Dla dzieci", 2);
+        Category fantasticHorror = new Category("Fantastyka i horror", 3);
+        Category history = new Category("Historia", 4);
+        Category criminalSensationThriller = new Category("Kryminał, sensacja, thriller", 5);
+        Category moralLiterature = new Category("Literatura obyczajowa", 6);
+        Category logs = new Category("Dzienniki", 10);
+        Category autobiography = new Category("Autobiografie", 11);
+        Category interviews = new Category("Wywiady", 12);
+        Category forYoungest = new Category("Dla najmłodszych", 21);
+        Category forTeenagers = new Category("Dla nastolatków", 22);
+        Category fantasy = new Category("Fantastyka", 31);
+        Category horror = new Category("Horror", 32);
+        Category sf = new Category("Sf", 33);
+        Category worldHistory = new Category("Historia świata", 41);
+        Category polishHistory = new Category("Historia Polski", 42);
+        Category criminal = new Category("Kryminał", 51);
+        Category senastion = new Category("Sensacja", 52);
+        Category thriller = new Category("Thriller", 53);
+        Category erotics = new Category("Erotyka", 61);
+        Category romance = new Category("Romans", 62);
 
-    public void settingCategory(){
-        biograf.addChildCategory(autobiograf);
-        biograf.addChildCategory(jurnal);
-        biograf.addChildCategory(Interview);
-        Comedy.addChildCategory(BlackComedy);
-        Comedy.addChildCategory(Criminal);
-        Comedy.addChildCategory(Romans);
-        Horror.addChildCategory(Criminal);
-
-        mainCategory.add(biograf);
-        mainCategory.add(Comedy);
-        mainCategory.add(Horror);
-
-        System.out.println(mainCategory.get(1).getName());
-    }
-
+        biography.addChildCategory(logs);
+        biography.addChildCategory(autobiography);
+        biography.addChildCategory(interviews);
+        forKids.addChildCategory(forYoungest);
+        forKids.addChildCategory(forTeenagers);
+        fantasticHorror.addChildCategory(fantasy);
+        fantasticHorror.addChildCategory(horror);
+        fantasticHorror.addChildCategory(sf);
+        history.addChildCategory(worldHistory);
+        history.addChildCategory(polishHistory);
+        criminalSensationThriller.addChildCategory(criminal);
+        criminalSensationThriller.addChildCategory(senastion);
+        criminalSensationThriller.addChildCategory(thriller);
+        moralLiterature.addChildCategory(erotics);
+        moralLiterature.addChildCategory(romance);
+        
+     }
 }
